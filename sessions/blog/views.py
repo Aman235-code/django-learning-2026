2c72ee8c3c007c99e79ece2370f1759ad9656d54
Aman Ahamed <amanahamed04@gmail.com>
2026-01-22 18:08:19 +0530

    Django Sessions

---
+++ sessions/blog/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def set_session(request):
+    request.session['username'] = 'aman'
+    request.session['course'] = 'Django full course'
+    return HttpResponse("Session data saved successfully")
+
+def get_session(request):
+    username = request.session.get('username', 'Guest')
+    course = request.session.get('course', 'Not enrolled')
+    return HttpResponse(f"Welcome : {username} You are learning : {course}")
+
+def delete_session(request):
+    # try:
+    #     del request.session['username']
+    #     del request.session['course']
+    # except KeyError:
+    #     pass
+    # return HttpResponse("Session data deleted successfully")
+
+    request.session.flush() # delete all session data
+    return HttpResponse("All session data deleted successfully.")
